1795c342dff130b803ac3f01085b91549dd12529
ashahir <ashahir@gmail.com>
2024-05-23 17:22:04 +0800

    3rd day afternoon

---
+++ app/Http/Controllers/InstitusiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kursus;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController;
+
+class InstitusiController extends BaseController
+{
+    //
+    public function senarai_institusi(Request $request)
+    {
+        $pusat = $request->pusat;
+        $institusis = Kursus::select('pusat')
+        ->where('pusat','like','%'.$pusat.'%')
+        ->distinct()->get();
+        return $this->sendResponse($institusis, 'ok...');
+    }
+
+    public function pusat_institusi(Request $request)
+    {
+        $pusat = $request->pusat;
+        $institusis = Kursus::select('kursus')
+        ->where('pusat', $pusat)->get();
+        return $this->sendResponse($institusis, 'ok...');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KursusController;
+use App\Http\Controllers\InstitusiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,4 +11,9 @@ Route::get('/user', function (Request $request) {
 
 Route::controller(KursusController::class)->group(function(){
     Route::get('senarai-kursus', 'senarai_kursus')->name('senarai-kursus');
+});
+
+Route::controller(InstitusiController::class)->group(function(){
+    Route::post('senarai-institusi', 'senarai_institusi')->name('senarai-institusi');
+    Route::post('pusat-institusi', 'pusat_institusi')->name('pusat-institusi');
 });
